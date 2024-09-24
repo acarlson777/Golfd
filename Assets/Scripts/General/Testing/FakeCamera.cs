8e12416f66d35e0eb1849614caa315f8b27b5e09
Cai Scheidler <scheidlercai@gmail.com>
2024-09-24 10:35:15 -0700

    It's Working.

--- Assets/Scripts/General/Testing/FakeCamera.cs
+++ Assets/Scripts/General/Testing/FakeCamera.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class FakeCamera : MonoBehaviour
 {
     [SerializeField] private Camera camera;
+    [SerializeField] private float convergeConstant;
 
     void Update()
     {
-        //transform.position += (camera.transform.position - transform.position + offset) / convergeConstant;
-        transform.position = camera.transform.position;
+        transform.position += (camera.transform.position - transform.position) / convergeConstant;
         transform.rotation = camera.transform.rotation;
     }
 }
