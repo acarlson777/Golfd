276cd71e077b81cb8286e8f702e6bdb1516f7a37
Cai Scheidler <scheidlercai@gmail.com>
2024-10-08 10:36:57 -0700

    Make mask for level transitioning within one world

--- Assets/Scripts/General/LevelHandling/LevelHandler.cs
+++ Assets/Scripts/General/LevelHandling/LevelHandler.cs
@@ -5,15 +5,19 @@ public class LevelHandler : MonoBehaviour
 {
     private float golfBallInHoleToLevelCompleteTransitionTime = 1f;
     public int par;
+    [SerializeField] private Vector3 animateInPosition;
+    private Vector3 animateOutPosition;
+
+    //https://www.youtube.com/watch?v=7vFwTt4isDY <-- Use this tutorial for masking
 
     private void Start()
     {
-        
+        animateOutPosition = transform.position;
     }
 
     public void AnimateIn()
     {
-
+        
     }
 
     public void AnimateOut()
@@ -25,5 +29,15 @@ public class LevelHandler : MonoBehaviour
     {
         yield return new WaitForSeconds(golfBallInHoleToLevelCompleteTransitionTime);
         WorldHandler.Instance.OnLevelCompleted();
+    }
+
+    private IEnumerator AnimateInCoroutine()
+    {
+        yield return null;
+    }
+
+    private IEnumerator AnimateOutCoroutine()
+    {
+        yield return null;
     }
 }
