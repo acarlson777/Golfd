baab803daf5f73a4f46093419184c5d5f5165c11
Cai Scheidler <scheidlercai@gmail.com>
2024-12-05 10:37:29 -0800

    yo

---
+++ Assets/Scripts/UI/UpdateDebugText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using TMPro;
+
+public class UpdateDebugText : MonoBehaviour
+{
+    [SerializeField] private GameObject debugTextObject;
+    private string debugText = "";
+    [SerializeField] WorldHandler worldHandler;
+
+    private void Update()
+    {
+        debugText = "";
+        if (worldHandler.GetCurrLevelHandler() == null) return;
+        debugText += "Level Height: " + worldHandler.GetCurrLevelHandler().LEVEL.transform.position.y + "\n";
+        debugText += "Floor Height: " + GameObject.FindGameObjectWithTag("WorldFloor").transform.position.y;
+
+        debugTextObject.GetComponent<TextMeshProUGUI>().text = debugText;
+    }
+}
