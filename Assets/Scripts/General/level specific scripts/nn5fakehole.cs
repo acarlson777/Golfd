d8df62447d012babdbab2e69c2cd38072987f997
Alex A <Alexanderaguilarmelgar@outlook.com>
2024-12-17 12:28:42 -0800

    updated nn5fakehole script again

--- Assets/Scripts/General/level specific scripts/nn5fakehole.cs
+++ Assets/Scripts/General/level specific scripts/nn5fakehole.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class nn5fakehole : MonoBehaviour
 {
     readonly GameObject b = GameObject.Find("GolfBallgen");
-    //float x = GameObject.Find("GolfBallgen").transform.position.x;
-    //float y = GameObject.Find("GolfBallgen").transform.position.y;
-    //float z = GameObject.Find("GolfBallgen").transform.position.z;
+    float x = GameObject.Find("GolfBallgen").transform.position.x;
+    float y = GameObject.Find("GolfBallgen").transform.position.y;
+    float z = GameObject.Find("GolfBallgen").transform.position.z;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("GolfBall"))
         {
-            other.gameObject.transform.position = new Vector3(b.transform.position.x, b.transform.position.y + (float) 0.4, b.transform.position.z); 
+            other.gameObject.transform.position = new Vector3(x, y + (float) 0.4, z); 
         }
         
         
