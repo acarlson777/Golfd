c26de6c03339d84e958c72586b92fae780f56a5b
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-01-15 10:07:13 -0800

    moving obstacle
    need a wait() equivalent in cs

---
+++ Assets/Scripts/General/level specific scripts/NN5/MovingPscript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MovingPscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+    // Update is called once per frame
+    void Update()
+
+    {
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
+        
+
+    }
+}
