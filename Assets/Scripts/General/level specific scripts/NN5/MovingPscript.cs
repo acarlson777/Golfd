03457ccdeafdc1e84ec40666c71c40b2c993d716
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-01-16 10:36:35 -0800

    fixed some stuff
    fixed MovingPscript.cs

--- Assets/Scripts/General/level specific scripts/NN5/MovingPscript.cs
+++ Assets/Scripts/General/level specific scripts/NN5/MovingPscript.cs
@@ -6,39 +6,53 @@ using UnityEngine;
 public class MovingPscript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float xint;
+    public bool plusfour;
+    public GameObject ObjectObstacle;
+   
     void Start()
     {
-        
+        xint = 0;
+        plusfour = false;
+        xint = ObjectObstacle.transform.position.x;
     }
 
-
+    
+    
     
     // Update is called once per frame
     void Update()
 
     {
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)0.1, 0, 0), Space.Self);
+        StartCoroutine(sleepCoroutine());
         
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        transform.Translate(new Vector3((float)-0.1, 0, 0), Space.Self);
-        
+    }
+
+    private IEnumerator sleepCoroutine()
+    {
+
+        yield return new WaitForSeconds(5);
+        Debug.Log("Waiting");
+        if (xint > 4)
+        {
+            plusfour = true;
+            Debug.Log("True");
+        }
+        if (xint < 1)
+        {
+            plusfour = false;
+            Debug.Log("false");
+        }
+        if (plusfour == false)
+        {
+            transform.Translate(new Vector3((float)0.05, 0, 0), Space.World);
+        }
+        if (plusfour == true)
+        {
+            transform.Translate(new Vector3((float)-0.05, 0, 0), Space.World);
+        }
+
+
 
     }
 }
