8ae6f360ce9902f426f25dbe503e9e9e7a735a82
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-01-31 10:15:38 -0800

    nn5fakehole script now tracks the location of the GolfBallGen gameobject

--- Assets/Scripts/General/level specific scripts/NN5/nn5fakehole.cs
+++ Assets/Scripts/General/level specific scripts/NN5/nn5fakehole.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class nn5fakehole : MonoBehaviour
 {
-
-   
+    public GameObject GolfBallGen;
+    public GameObject GolfBall;
     
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("GolfBall"))
         {
-            other.gameObject.transform.position = new Vector3((float)0.3430001,(float)0.1906604,(float) -0.5275999); 
+            // teleports the Golf ball to the Golf Ball generator
+            GolfBall.transform.position = GolfBallGen.transform.position;
+            
         }
-        
-        
     }
 }
