04f7196437f914f0773a6c8d0e9e415dee579c90
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-02-04 10:08:10 -0800

    MW3 edit

---
+++ Assets/Scripts/General/level specific scripts/NN5/Respawn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject GolfBallGen1;
+    public GameObject GolfBall;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GolfBall"))
+        {
+            // teleports the Golf ball to the Golf Ball generator
+            GolfBall.transform.position = GolfBallGen1.transform.position;
+
+        }
+    }
+}
