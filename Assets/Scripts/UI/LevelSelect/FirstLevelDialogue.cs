fcc0d92bdfef543fa1d4eab0b82a3e2db2d1832d
Cai Scheidler <scheidlercai@gmail.com>
2025-02-14 10:35:54 -0800

    FINISH 1 Time dialogues

---
+++ Assets/Scripts/UI/LevelSelect/FirstLevelDialogue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstLevelDialogue : MonoBehaviour
+{
+    [SerializeField] private DialogueWrapper dialogueWrapper;
+    [SerializeField] bool resetFirstTimeStatus;
+
+    void Start()
+    {
+        if (resetFirstTimeStatus)
+        {
+            PlayerPrefs.SetString("isFirstTimeInLevel", "true");
+        }
+
+        if (PlayerPrefs.GetString("isFirstTimeInLevel", "true").Equals("true"))
+        {
+            dialogueWrapper.StartDialogueSequence("INTRO-SEQUENCE", () => { });
+        }
+        PlayerPrefs.SetString("isFirstTimeInLevel", "false");
+    }
+}
+
