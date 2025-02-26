54bf4a5b9b1801e0a883b00fcae963359c93076a
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-02-26 10:05:21 -0800

    NN3 and NN6 update

---
+++ Assets/Scripts/UI/LevelSelect/FirstLevelDialogue.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstLevelDialogue : MonoBehaviour
+{
+    [SerializeField] private DialogueWrapper dialogueWrapper;
+    [SerializeField] bool resetFirstTimeStatus;
+    [SerializeField] LevelPlacementHandler levelPlacementHandler;
+
+    void Start()
+    {
+        if (resetFirstTimeStatus)
+        {
+            PlayerPrefs.SetString("isFirstTimeInLevel", "true");
+        }
+
+        if (PlayerPrefs.GetString("isFirstTimeInLevel", "true").Equals("true"))
+        {
+            levelPlacementHandler.canPlaceLevel = false;
+            dialogueWrapper.StartDialogueSequence("INTRO-SEQUENCE", () => {levelPlacementHandler.canPlaceLevel = true; });
+        }
+        PlayerPrefs.SetString("isFirstTimeInLevel", "false");
+    }
+}
+
---
+++ Assets/Scripts/UI/TitleScreenDialogue.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScreenDialogue : MonoBehaviour
+{
+    [SerializeField] private DialogueWrapper dialogueWrapper;
+    [SerializeField] bool resetFirstTimeStatus;
+
+    void Start()
+    {
+        if (resetFirstTimeStatus)
+        {
+            PlayerPrefs.SetString("FirstTitleScreen", "true");
+        }
+
+        if (PlayerPrefs.GetString("FirstTitleScreen", "true").Equals("true"))
+        {
+            dialogueWrapper.StartDialogueSequence("INTRO-SEQUENCE", () => { });
+        }
+        PlayerPrefs.SetString("FirstTitleScreen", "false");
+    }
+}
