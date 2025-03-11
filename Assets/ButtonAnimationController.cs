a9578753ba4ca5b04cce7ad4d4f67cf4675e28ea
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-03-11 10:00:25 -0700

    commit test on main copy

--- Assets/ButtonAnimationController.cs
+++ Assets/ButtonAnimationController.cs
@@ -16,6 +16,14 @@ public class ButtonAnimationController : MonoBehaviour{
     private bool sfx;
     private bool music;
 
+    private bool canTapSFX = true;
+    private bool canTapMUSIC = true;
+
+    private void Start()
+    {
+   
+    }
+
     void OnEnable(){
 
         if(buttonMusic == null || buttonSfx == null){
@@ -34,6 +42,9 @@ public class ButtonAnimationController : MonoBehaviour{
         sfx = PlayerPrefs.GetInt("sfx") == 1;
         music = PlayerPrefs.GetInt("music") == 1;
 
+        print(sfx);
+        print(music);
+
         sfxAnimator.SetBool("isOn", sfx);
         musicAnimator.SetBool("isOn", music);
 
@@ -42,16 +53,44 @@ public class ButtonAnimationController : MonoBehaviour{
     }
 
     public void OnSfxButtonClick(){
-            sfx = !sfx;
-            sfxAnimator.SetBool("isOn", sfx);
-            // soundHandler.TapSfxButton();
-            Debug.Log("called sfx sound handler and set to " + sfx);
+        if (!canTapSFX)
+        {
+            return;
+        }
+        sfx = !sfx;
+        sfxAnimator.SetBool("isOn", sfx);
+        sfxAnimator.SetTrigger("isAnimating");
+        canTapSFX = false;
+        StartCoroutine(WaitBeforeTapAgainSFX());
+
+        // soundHandler.TapSfxButton();
+        Debug.Log("called sfx sound handler and set to " + sfx);
     }
 
     public void OnMusicButtonClick(){
-            music = !music;
-            musicAnimator.SetBool("isOn", music);
-            // soundHandler.TapMusicButton();
-            Debug.Log("called music sound handler and set to " + music);
+        if (!canTapMUSIC)
+        {
+            return;
+        }
+        music = !music;
+        musicAnimator.SetBool("isOn", music);
+        musicAnimator.SetTrigger("isAnimating");
+        canTapMUSIC = false;
+        StartCoroutine(WaitBeforeTapAgainMUSIC());
+
+        // soundHandler.TapMusicButton();
+        Debug.Log("called music sound handler and set to " + music);
+    }
+
+    public IEnumerator WaitBeforeTapAgainSFX()
+    {
+        yield return new WaitForSeconds(.3333f);
+        canTapSFX = true;
+    }
+
+    public IEnumerator WaitBeforeTapAgainMUSIC()
+    {
+        yield return new WaitForSeconds(.3333f);
+        canTapMUSIC = true;
     }
 }
---
+++ Assets/Scripts/General/level specific scripts/NN3/LightsOutA.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightsOutA : MonoBehaviour
+{
+    public GameObject Ball_Light;
+    public GameObject LightA1;
+    public GameObject LightA2;
+
+    void Start()
+    {
+        Ball_Light.SetActive(false);
+        
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("GolfBall"))
+        {
+            Ball_Light.SetActive(true);
+            LightA1.SetActive(false);
+            LightA2.SetActive(false);
+        }
+    }
+}
