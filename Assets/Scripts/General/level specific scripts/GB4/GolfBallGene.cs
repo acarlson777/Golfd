7937027cc7124b24a40570a8b86a5dd4ef715cf4
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-04-09 10:06:20 -0700

    Goldberg4 shenanigans
    new working conveyor with golfball

---
+++ Assets/Scripts/General/level specific scripts/GB4/GolfBallGene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBallGene : MonoBehaviour
+{
+    public GameObject GolfBalConveyorEnt;
+    public GameObject GolfBallnon;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "GolfBallnon")
+        {
+            // teleports the Golf ball to the Golf Ball generator
+            GolfBallnon.transform.position = GolfBalConveyorEnt.transform.position;
+
+        }
+    }
+}
