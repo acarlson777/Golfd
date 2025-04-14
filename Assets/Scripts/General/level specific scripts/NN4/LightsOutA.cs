fa16db53577db705939f2da11517ecc189ff3ba7
Alex A <Alexanderaguilarmelgar@outlook.com>
2025-04-14 10:36:32 -0700

    lightsoutA1 edit and improvements

--- Assets/Scripts/General/level specific scripts/NN4/LightsOutA.cs
+++ Assets/Scripts/General/level specific scripts/NN4/LightsOutA.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightsOutA : MonoBehaviour
 {
+    public GameObject GolfBall;
     public GameObject Ball_Light;
     public GameObject LightA1;
     public GameObject LightA2;
-
+    public Material Glow;
+    public Material Lit;
     void Start()
     {
         Ball_Light.SetActive(false);
+        GolfBall.GetComponent<MeshRenderer>().material = Lit;
     }
 
 
@@ -18,9 +22,11 @@ public class LightsOutA : MonoBehaviour
     {
         if (other.gameObject.CompareTag("GolfBall"))
         {
-            Ball_Light.SetActive(true);
+            GolfBall.GetComponent<MeshRenderer>().material = Glow;
+            
             LightA1.SetActive(false);
             LightA2.SetActive(false);
+
         }
     }
 }
